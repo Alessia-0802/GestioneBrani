efe88336085d38b897343582e305ef77e90735c6
Edoardo Giorgetti <EDOARDO.GIORGETTI@local.ittcesena.it>
2025-10-31 09:37:50 +0100

    R0.1: prima versione classe CD

--- GestioneBrani/GestioneBrani/Cd.cs
+++ GestioneBrani/GestioneBrani/Cd.cs
@@ -8,5 +8,44 @@ namespace GestioneBrani
 {
     class Cd
     {
+        private string _titolo;
+        private string _autore;
+        private List<Brano> _brani;
+
+        public Cd(string titolo, string autore)
+        {
+            _titolo = titolo;
+            _autore = autore;
+            _brani = new List<Brano>();
+        }
+        public string GetTitolo()
+        {
+            return _titolo;
+        }
+        public void SetTitolo(string titolo)
+        {
+            _titolo = titolo;
+        }
+        public string GetAutore()
+        {
+            return _autore;
+        }
+        public void SetAutore(string autore)
+        {
+            _autore = autore;
+        }
+        public void AggiungiBrano(Brano b)
+        {
+            _brani.Add(b);
+        }
+        public double Durata()
+        {
+            double totale = 0;
+            foreach (Brano b in _brani)
+            {
+                totale += b.GetDurata();
+            }
+            return totale;
+        }
     }
 }
