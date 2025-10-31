0c15b90eab538eff89c1f59b60c51d902aa7a942
Alessia Gentili <ALESSIA.GENTILI@local.ittcesena.it>
2025-10-31 09:43:12 +0100

    Aggiunto proprietà get e set di autore, titolo e durata. Aggiunto costruttore

--- GestioneBrani/GestioneBrani/Brano.cs
+++ GestioneBrani/GestioneBrani/Brano.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GestioneBrani
 {
-    class Brano
+    class Brano 
     {
+        public string titolo { get; set; }
+        public string autore { get; set; }
+        public double durata {  get; set; }
+
+        public Brano(string newTitolo, string newAutore, double newDurata)
+        {
+            newTitolo = titolo;
+            newAutore = autore;
+            newDurata = durata;
+
+        }
     }
+
+    
 }
