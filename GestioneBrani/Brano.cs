885bcda11f7b748e77bdf5419ae5447eff065b57
Sofia Ghirardi <SOFIA.GHIRARDI@local.ittcesena.it>
2025-10-31 09:44:09 +0100

    prima versione

--- GestioneBrani/Brano.cs
+++ GestioneBrani/Brano.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GestioneBrani
 {
-    class Brano
+    public class Brano
     {
     }
 }
--- GestioneBrani/Cd.cs
+++ GestioneBrani/Cd.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GestioneBrani
 {
-    class Cd
+    public class Cd
     {
     }
 }
