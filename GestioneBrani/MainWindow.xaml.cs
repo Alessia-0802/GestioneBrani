7aa40f985fea85c040d338576344456c15ba2893
Sofia Ghirardi <SOFIA.GHIRARDI@local.ittcesena.it>
2025-11-07 08:50:27 +0100

    WPF

--- GestioneBrani/MainWindow.xaml.cs
+++ GestioneBrani/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace GestioneBrani
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
