cb1b5b7f30541a4642e9dbdfed2b71986976b04d
Alessia Gentili <ALESSIA.GENTILI@local.ittcesena.it>
2025-11-07 08:46:11 +0100

    Aggiunti metodi ToString e ShortSong e commenti. Versione finale del branch brano

--- GestioneBrani/GestioneBrani/Brano.cs
+++ GestioneBrani/GestioneBrani/Brano.cs
@@ -10,10 +10,12 @@ namespace GestioneBrani
 {
     class Brano 
     {
+        // Permettono di leggere e modificare direttamente i valori 
         public string titolo { get; set; }
         public string autore { get; set; }
         public double durata {  get; set; }
 
+        // Costruttore della classe: iniziaòlizza uhn nuovo oggetto brano
         public Brano(string newTitolo, string newAutore, double newDurata)
         {
             newTitolo = titolo;
@@ -21,7 +23,18 @@ namespace GestioneBrani
             newDurata = durata;
 
         }
-    }
 
-    
+        // Metodo che restituisce una descrizione testuale dell'oggetto Brano.
+        public override string ToString()   // override, per sovrascivere il metodo ToString().
+        {
+            return $"Titolo: {titolo}. Autore: {autore}. Durata: {durata}";     //Il segno $, è usato per poter mettere all'interno di una strigna direttamente le variabili tramite le parentesi graffe.
+        }
+
+        // Metodo che verifica se la durata del brano è minore della soglia.
+        public bool ShortSong (double sogliaDurata)
+        {
+            // Restituisce true se il valore della durata è maggiore della soglia, altrimenti false.
+            return durata < sogliaDurata;
+        }
+    }
 }
